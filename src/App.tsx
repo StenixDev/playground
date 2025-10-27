d38771c51c9c95b5bec9146ca9e088afda392f6c
StenixDev <stenix.dev@gmail.com>
2025-10-27 15:58:58 +0800

    Starter Files

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-3xl text-amber-400">Playground!</h1>
+
+      <Button>Click</Button>
+    </div>
+  );
+}
+export default App;
