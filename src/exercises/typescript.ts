235fc63c5a6e4430e3015fa6221408e6e64a9ca3
StenixDev <stenix.dev@gmail.com>
2025-10-28 17:58:12 +0800

    just  typing

--- src/exercises/typescript.ts
+++ src/exercises/typescript.ts
@@ -68,4 +68,50 @@ function printUserInfo(user: User) {
   return `Name: ${user.name} Age: ${user.age} Location: ${user.location}`;
 }
 
+// optional properties
+
 console.log(printUserInfo({ name: "stenix", age: 34, location: "phil" }));
+
+type User2 = {
+  name: string;
+  age?: number;
+  readonly location: string;
+};
+
+const userInput: User2 = {
+  name: "Stenix",
+  location: "Phil",
+};
+
+userInput.name = "stigward";
+
+type UserInfo = {
+  first: string;
+  last: string;
+  age: number;
+};
+
+type AccountDetails = {
+  email: string;
+  password: string;
+};
+
+type User3 = UserInfo & AccountDetails;
+
+const stenix: User3 = {
+  first: "Step",
+  last: "hen",
+  age: 34,
+  email: "connect@stenix.dev",
+  password: "stenixpass",
+};
+
+const items2: (number | string)[] = [1, "stenix", 12];
+
+enum WeatherConditions {
+  Sunny = "sunny",
+  Cloudy = "cloudy",
+  Rainy = "rainy",
+}
+
+console.log(WeatherConditions.Cloudy);
