bfb428eaade84745102903fb13353e9a7df2aabe
StenixDev <stenix.dev@gmail.com>
2025-11-08 07:36:15 +0800

    useActionState

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,6 @@
-import MyForm from "./components/AdvancedForm";
-import Form from "./components/Form";
+import Count from "./components/Count";
 
 function App() {
-  return (
-    <div>
-      <MyForm />
-    </div>
-  );
+  return <Count />;
 }
 export default App;
